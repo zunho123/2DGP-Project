f45be255915649223866ea8ea0b82901ed11b2a5
zunho123 <zunho8331@tukorea.ac.kr>
2025-11-29 16:42:33 +0900

    스노우 공격 모션 추가

--- project/snow.py
+++ project/snow.py
@@ -1,11 +1,6 @@
 from pico2d import *
 
-SN_IDLE, SN_DEAD = 0, 1
-
-
-from pico2d import *
-
-SN_IDLE, SN_DEAD = 0, 1
+SN_IDLE, SN_ATTACK, SN_DEAD = 0, 1, 2
 
 
 class Snow:
@@ -13,6 +8,7 @@ class Snow:
         self.stage = stage
         self.img_idle = load_image('snow_idle.png')
         self.img_dead = load_image('snow_dead.png')
+        self.img_attack = load_image('snow_att.png')
 
         self.data_idle = dict(
             lefts=[6, 50, 92, 136, 179, 223, 266, 311, 355, 400, 443, 487, 530, 574],
@@ -26,7 +22,13 @@ class Snow:
             pad=0
         )
 
-        for d in (self.data_idle, self.data_dead):
+        self.data_attack = dict(
+            lefts=[9, 84, 200, 313, 427, 528, 619, 701, 784, 872, 959, 1042, 1132, 1216],
+            widths=[56, 97, 99, 99, 88, 83, 79, 79, 80, 76, 75, 76, 75, 76],
+            pad=1
+        )
+
+        for d in (self.data_idle, self.data_dead, self.data_attack):
             pad = d.get('pad', 0)
             eff = [max(1, w - 2 * pad) for w in d['widths']]
             d['aw'] = sum(eff) / len(eff)
@@ -34,18 +36,34 @@ class Snow:
         self.char_scale = scale
         self.x = x
         self.ground_off = 0
-        self.y = stage.ground_y + self.ground_off + 17.5
+        self.y_idle = stage.ground_y + self.ground_off + 17.5
+        self.y_attack = stage.ground_y + self.ground_off + 10
+        self.y = self.y_idle
 
         self.state = SN_IDLE
+
         self.frame = 0
         self.tacc = 0.0
         self.idle_gap = 0.08
+
+        self.attack_frame = 0
+        self.attack_tacc = 0.0
+        self.attack_gap = 0.06
 
         self.dead_frame = 0
         self.dead_tacc = 0.0
         self.dead_gap = 0.08
 
         self.visible = True
+
+    def start_attack(self, px, pdir):
+        self.visible = True
+        self.state = SN_ATTACK
+        self.attack_frame = 0
+        self.attack_tacc = 0.0
+        offset = 70.0 * self.char_scale
+        self.x = px + pdir * offset
+        self.y = self.stage.ground_y + self.ground_off + 10.0
 
     def die(self):
         if self.state == SN_DEAD:
@@ -61,6 +79,21 @@ class Snow:
             while self.tacc >= self.idle_gap:
                 self.tacc -= self.idle_gap
                 self.frame = (self.frame + 1) % len(self.data_idle['widths'])
+
+        elif self.state == SN_ATTACK:
+            self.attack_tacc += dt
+            while self.attack_tacc >= self.attack_gap:
+                self.attack_tacc -= self.attack_gap
+                if self.attack_frame < len(self.data_attack['widths']) - 1:
+                    self.attack_frame += 1
+                else:
+                    self.state = SN_IDLE
+                    self.attack_frame = 0
+                    self.attack_tacc = 0.0
+                    self.frame = 0
+                    self.y = self.stage.ground_y + self.ground_off + 17.5
+                    self.x -= 12.5
+                    break
 
         elif self.state == SN_DEAD:
             if not self.visible:
@@ -95,6 +128,11 @@ class Snow:
         if self.state == SN_IDLE:
             self.stage.draw_frame(
                 self.img_idle, self.data_idle, self.frame,
+                self.x, self.y, self.char_scale, flip_flag
+            )
+        elif self.state == SN_ATTACK:
+            self.stage.draw_frame(
+                self.img_attack, self.data_attack, self.attack_frame,
                 self.x, self.y, self.char_scale, flip_flag
             )
         else:
